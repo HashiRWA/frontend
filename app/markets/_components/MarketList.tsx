7f24e3602f337bd484aca160e968138b423f3219
Heemank Verma <heemankv@gmail.com>
2024-06-16 08:18:06 +0530

    update: merge fixed

--- app/markets/_components/MarketList.tsx
+++ app/markets/_components/MarketList.tsx
@@ -6,12 +6,13 @@ import { Market } from "@/types";
 import { ArrowDown, ArrowUp, Wallet } from "lucide-react";
 import { useContext } from "react";
 import { BlockChainContext } from "@/context/BlockChainContext";
-import Loader from "@/components/Loader";
 import { getTokenDetails } from "@/constants";
+import Loader from "@/components/Loader";
+
 
 const MarketList = ({ markets }: { markets: Market[] }) => {
   const { pools, loading, signer, connect } = useContext(BlockChainContext);
-  
+
   return (
     <div className="mx-auto flex h-full w-[90%] flex-col items-center rounded-lg bg-white py-4 shadow-2xl">
       <div className="mb-2 flex w-full justify-between border-b p-3 px-8 text-sm font-medium uppercase text-[#374950]">
@@ -37,8 +38,7 @@ const MarketList = ({ markets }: { markets: Market[] }) => {
                       <Image src="/USDC.svg" alt="" width={30} height={30} />
                       <Image src="/USDC.svg" alt="" width={30} height={30} />
                     </div>
-                    {market?.asset?.substring(0, 20)} /{" "}
-                    {market?.collateral?.substring(0, 20)}
+                    {getTokenDetails(market?.asset) ? getTokenDetails(market?.asset)?.symbol : market?.asset?.substring(0,20)} / {getTokenDetails(market?.collateral) ? getTokenDetails(market?.collateral)?.symbol : market?.collateral?.substring(0,20)}
                   </span>
 
                   <span className="w-[9%]">{market?.debtinterestrate}%</span>
@@ -58,63 +58,25 @@ const MarketList = ({ markets }: { markets: Market[] }) => {
                     <div className="flex w-max flex-col rounded-lg p-2">
                       <span className="flex gap-x-3">
                         <span className="font-bold">Total Assets:</span>
-                        <span>{market?.totalAssets}</span>
+                        <span>{Number(market?.totalAssets) / (10 ** getTokenDetails(market?.asset).decimals  ) } {getTokenDetails(market?.asset) ? getTokenDetails(market?.asset)?.symbol : market?.asset?.substring(0,20)}</span>
                       </span>
 
                       <span className="flex gap-x-3">
                         <span className="font-bold">Total Collateral:</span>
-                        <span>${market?.totalCollateral}</span>
+                        <span>{Number(market?.totalCollateral) / (10 ** getTokenDetails(market?.collateral).decimals  ) } {getTokenDetails(market?.collateral) ? getTokenDetails(market?.collateral)?.symbol : market?.collateral?.substring(0,20)}</span>
                       </span>
 
                       <div className="flex items-center gap-2">
                         <span className="font-bold">Strike Price:</span>
                         <span className="flex gap-2 text-xs">
                           <span>{market?.strikeprice}</span>
-                          {market?.asset?.substring(0, 20)}/
-                          {market?.collateral?.substring(0, 20)}
+                          <span>{getTokenDetails(market?.collateral) ? getTokenDetails(market?.collateral)?.symbol : market?.collateral?.substring(0,20)} / {getTokenDetails(market?.asset) ? getTokenDetails(market?.asset)?.symbol : market?.asset?.substring(0,20)} </span>
                         </span>
                       </div>
                     </div>
                   </div>
 
-							<span className="flex w-2/4 items-center gap-1 font-medium">
-								<div className="flex -space-x-2">
-									<Image src="/USDC.svg" alt="" width={30} height={30} />
-									<Image src="/USDC.svg" alt="" width={30} height={30} />
-								</div>
-								{getTokenDetails(market?.asset) ? getTokenDetails(market?.asset)?.symbol : market?.asset?.substring(0,20)} / {getTokenDetails(market?.collateral) ? getTokenDetails(market?.collateral)?.symbol : market?.collateral?.substring(0,20)}
-							</span>
-
-							<span>{market?.debtinterestrate}%</span>
-							<span>{market?.lendinterestrate}%</span>
-							<span>{market?.overcollateralizationfactor*100}</span>
-							<span>{new Date(market?.maturationdate * 1000).toLocaleDateString()}</span> 
-						</div>
-
-						<div className="flex gap-3 text-[0.8rem] border-[.5px] border-teal-700 w-[95%] my-4 px-5 rounded-md">
-
-							<div className="flex w-2/4 gap-x-5 ">
-								<div className="flex flex-col p-2 rounded-lg w-max">
-									<span className="flex gap-x-3 ">
-										<span className="font-bold">Total Assets:</span>
-										<span>{Number(market?.totalAssets) / (10 ** getTokenDetails(market?.asset).decimals  ) } {getTokenDetails(market?.asset) ? getTokenDetails(market?.asset)?.symbol : market?.asset?.substring(0,20)}</span>
-									</span>
-
-									<span className="flex gap-x-3">
-										<span className="font-bold">Total Collateral:</span>
-										<span>{Number(market?.totalCollateral) / (10 ** getTokenDetails(market?.collateral).decimals  ) } {getTokenDetails(market?.collateral) ? getTokenDetails(market?.collateral)?.symbol : market?.collateral?.substring(0,20)}</span>
-									</span>
-
-									<div className="flex items-center gap-2 ">
-										<span className="font-bold">Strike Price:</span>
-										<span className="flex gap-2 text-xs">
-											<span>{market?.strikeprice}</span>
-											<span>{getTokenDetails(market?.collateral) ? getTokenDetails(market?.collateral)?.symbol : market?.collateral?.substring(0,20)} / {getTokenDetails(market?.asset) ? getTokenDetails(market?.asset)?.symbol : market?.asset?.substring(0,20)} </span>
-										</span>
-									</div>
-								</div>
-							</div>
-              <div className="flex flex-1 items-center justify-center gap-5">
+                  <div className="flex flex-1 items-center justify-center gap-5">
                     <Link
                       href={`/lend/${market?.id}`}
                       className="flex items-center gap-3 rounded bg-[#00494d] px-8 py-2 font-medium text-[#E0F6EC] ring-inset ring-[#00494d] transition-colors duration-150 ease-in-out hover:bg-[#E0F6EC] hover:text-[#00494d] hover:ring-[1px]">
